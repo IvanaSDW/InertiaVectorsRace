1508229cad291b1cda53dbb01c78d7f8c5890fe2
ivanasdw <ivanasdw@gmail.com>
2016-05-07 15:31:31 -0500

    Version Alpha muy basica

---
+++ app/src/main/java/UsingSurfaceView/InertiaSurfaceView.java
@@ -0,0 +1,18 @@
+package UsingSurfaceView;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.SurfaceView;
+
+/**
+ * Created by aurel on 5/7/2016.
+ */
+public class InertiaSurfaceView  extends SurfaceView{
+    public InertiaSurfaceView(Context context) {
+        super(context);
+    }
+
+    public InertiaSurfaceView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+}
